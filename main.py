30ed8c0b29f4df09ed455a26c4a92a5d1be94648
andrii-trebukh <andrew.trebukh@gmail.com>
2023-12-24 12:02:34 +0200

    commit #00

---
+++ main.py
@@ -0,0 +1,134 @@
+from collections import UserDict
+from datetime import datetime
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.__value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        self.__value = new_value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    # def __init__(self, value):
+    #     if len(value) != 10 or re.search(r"\D", value):
+    #         raise ValueError("Incorrect phone number format")
+    #     super().__init__(value)
+    @Field.value.setter
+    def value(self, new_value):
+        if len(new_value) != 10 or re.search(r"\D", new_value):
+            raise ValueError("Incorrect phone number format")
+        Field.value.fset(self, new_value)
+
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, new_value):
+        try:
+            birthday = datetime.strptime(new_value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError(
+                "Incorrect birthday data format. Please use dd.mm.yyyy pattern"
+            )
+        if birthday > datetime.today():
+            raise ValueError(f"Birthday date '{new_value}' is in the future")
+        Field.value.fset(self, birthday)
+
+    def __str__(self):
+        return str(self.value.strftime("%d.%m.%Y"))
+
+
+class Record:
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.phones = []
+        if birthday is not None:
+            self.birthday = Birthday(birthday)
+        else:
+            self.birthday = None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, rm_phone):
+        for phone in self.phones:
+            if phone.value == rm_phone:
+                self.phones.remove(phone)
+                return
+        raise ValueError(f'Phone "{rm_phone}" does not exist')
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, _ in enumerate(self.phones):
+            if self.phones[i].value == old_phone:
+                self.phones[i].value = new_phone
+                return
+        raise ValueError(f'Phone "{old_phone}" does not exist')
+
+    def find_phone(self, find_phone):
+        for phone in self.phones:
+            if phone.value == find_phone:
+                return phone
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, " \
+            f"phones: {'; '.join(p.value for p in self.phones)}, " \
+            f"birthday: {self.birthday}"
+
+    def days_to_birthday(self):
+        if self.birthday is None:
+            return None
+
+        today = datetime.today()
+        # truncating hours, minutes etc.
+        today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        current_year = int(today.strftime("%Y"))
+        this_year_bday = self.birthday.value.replace(year=current_year)
+
+        if this_year_bday < today:
+            this_year_bday = self.birthday.value.replace(year=current_year + 1)
+
+        delta = this_year_bday - today
+        return delta.days
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        name = record.name.value
+        if self.data.get(name):
+            raise ValueError(f'Name "{name}" already exist')
+        self.data[name] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if self.data.get(name):
+            self.data.pop(name)
+
+    def iterator(self, n=1):
+        if n <= 0:
+            raise ValueError("Incorrect N value. N shuld be greater than 0")
+        output = {}
+        for index, (key, val) in enumerate(self.data.items()):
+            output[key] = val
+            if (index + 1) % n == 0:
+                yield output
+                output = {}
+        if output:
+            yield output
